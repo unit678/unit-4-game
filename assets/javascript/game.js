233f6a0ae06dcff35833f1a55b3475fa3c628f58
Garrion Martin <unit678@gmail.com>
2018-09-11 16:10:38 -0400

    final

---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+//this is wehere the computer will select a number to begin the game
+
+var randomNumber = 18 + Math.floor(Math.random() * 120);
+
+//this id link will allow the number to show
+$('#Guess').html(randomNumber);
+console.log(randomNumber);
+//the computer selects a number between 1 and all the variables for the game.
+var blueCrystal = 1 + Math.floor(Math.random() * 10);
+var redCrystal = 1 + Math.floor(Math.random() * 10);
+var yellowCrystal = 1 + Math.floor(Math.random() * 10);
+var greenCrystal = 1 + Math.floor(Math.random() * 10);
+
+var userTotal = 0;
+var wins = 0;
+var losses = 0;
+$('#Guess').html(userTotal);
+$('#wins').html(wins);
+$('#losses').html(losses);
+
+//reset everything once you have won the game or loss
+
+var reset = function () {
+  userTotal = 0;
+  randomNumber = 18 + Math.floor(Math.random() * 120);
+  blueCrystal = 1 + Math.floor(Math.random() * 10);
+  redCrystal = 1 + Math.floor(Math.random() * 10);
+  yellowCrystal = 1 + Math.floor(Math.random() * 10);
+  greenCrystal = 1 + Math.floor(Math.random() * 10);
+  console.log(randomNumber);
+  $('#Guess').html(randomNumber);
+  $('#totalNum').html(userTotal);
+  $('#wins').html(wins);
+  $('#losses').html(losses);
+};
+
+
+//the rules of the game when you are playing with the if and else statments.
+var rules = function () {
+  if (userTotal == randomNumber) {
+    wins++;
+    $('#wins').html(wins);
+    reset();
+  } else if (userTotal > randomNumber) {
+    losses++;
+    $('#losses').html(losses);
+    reset();
+  }
+};
+
+//all the crystals are here when they are pressed giving the random number.
+
+$('#blue').on('click', function () {
+  userTotal = userTotal + blueCrystal;
+  console.log(userTotal);
+  $('#totalNum').html(userTotal);
+  rules();
+});
+
+$('#red').on('click', function () {
+  userTotal = userTotal + redCrystal;
+  console.log(userTotal);
+  $('#totalNum').html(userTotal);
+  rules();
+});
+
+$('#yellow').on('click', function () {
+  userTotal = userTotal + yellowCrystal;
+  console.log(userTotal);
+  $('#totalNum').html(userTotal);
+  rules();
+});
+
+$('#green').on('click', function () {
+  userTotal = userTotal + greenCrystal;
+  console.log(userTotal);
+  $('#totalNum').html(userTotal);
+  rules();
+});
+
+//the end
