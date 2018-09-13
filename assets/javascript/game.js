dea349d36162f3169cd73d889c4dadde1a5be562
Garrion Martin <unit678@gmail.com>
2018-09-12 21:47:33 -0400

    changes to final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 var randomNumber = 18 + Math.floor(Math.random() * 120);
 
 //this id link will allow the number to show
-$('#Guess').html(randomNumber);
+$('#numGuess').html(randomNumber);
 console.log(randomNumber);
 //the computer selects a number between 1 and all the variables for the game.
 var blueCrystal = 1 + Math.floor(Math.random() * 10);
@@ -11,39 +11,39 @@ var redCrystal = 1 + Math.floor(Math.random() * 10);
 var yellowCrystal = 1 + Math.floor(Math.random() * 10);
 var greenCrystal = 1 + Math.floor(Math.random() * 10);
 
-var userTotal = 0;
+var scoreTracker = 0;
 var wins = 0;
 var losses = 0;
-$('#Guess').html(userTotal);
-$('#wins').html(wins);
-$('#losses').html(losses);
+$('#yourScore').text(scoreTracker);
+$('#wins').text(wins);
+$('#losses').text(losses);
 
 //reset everything once you have won the game or loss
 
 var reset = function () {
-  userTotal = 0;
+  scoreTracker = 0;
   randomNumber = 18 + Math.floor(Math.random() * 120);
   blueCrystal = 1 + Math.floor(Math.random() * 10);
   redCrystal = 1 + Math.floor(Math.random() * 10);
   yellowCrystal = 1 + Math.floor(Math.random() * 10);
   greenCrystal = 1 + Math.floor(Math.random() * 10);
   console.log(randomNumber);
-  $('#Guess').html(randomNumber);
-  $('#totalNum').html(userTotal);
-  $('#wins').html(wins);
-  $('#losses').html(losses);
+  $('#numGuess').text(randomNumber);
+  $('#yourScore').text(scoreTracker);
+  $('#wins').text(wins);
+  $('#losses').text(losses);
 };
 
 
 //the rules of the game when you are playing with the if and else statments.
 var rules = function () {
-  if (userTotal == randomNumber) {
+  if (scoreTracker == randomNumber) {
     wins++;
-    $('#wins').html(wins);
+    $('#wins').text(wins);
     reset();
-  } else if (userTotal > randomNumber) {
+  } else if (scoreTracker > randomNumber) {
     losses++;
-    $('#losses').html(losses);
+    $('#losses').text(losses);
     reset();
   }
 };
@@ -51,30 +51,30 @@ var rules = function () {
 //all the crystals are here when they are pressed giving the random number.
 
 $('#blue').on('click', function () {
-  userTotal = userTotal + blueCrystal;
-  console.log(userTotal);
-  $('#totalNum').html(userTotal);
+  scoreTracker = scoreTracker + blueCrystal;
+  console.log(scoreTracker);
+  $('#yourScore').text(scoreTracker);
   rules();
 });
 
 $('#red').on('click', function () {
-  userTotal = userTotal + redCrystal;
-  console.log(userTotal);
-  $('#totalNum').html(userTotal);
+  scoreTracker = scoreTracker + redCrystal;
+  console.log(scoreTracker);
+  $('#yourScore').text(scoreTracker);
   rules();
 });
 
 $('#yellow').on('click', function () {
-  userTotal = userTotal + yellowCrystal;
-  console.log(userTotal);
-  $('#totalNum').html(userTotal);
+  scoreTracker = scoreTracker + yellowCrystal;
+  console.log(scoreTracker);
+  $('#yourScore').text(scoreTracker);
   rules();
 });
 
 $('#green').on('click', function () {
-  userTotal = userTotal + greenCrystal;
-  console.log(userTotal);
-  $('#totalNum').html(userTotal);
+  scoreTracker = scoreTracker + greenCrystal;
+  console.log(scoreTracker);
+  $('#yourScore').text(scoreTracker);
   rules();
 });
 
